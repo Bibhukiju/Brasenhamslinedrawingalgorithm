660cc5d7115dd8fee075b48ac04e5901343009c9
Bibhukiju <46820449+Bibhukiju@users.noreply.github.com>
2020-02-11 21:22:16 +0545

    Add files via upload
    watermark + delay function added

---
+++ brasenhams/main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<graphics.h>
+using namespace std;
+
+void drawline(int x0, int y0, int x1, int y1)
+{
+    int dx, dy, p, x, y;
+
+	dx=x1-x0;
+	dy=y1-y0;
+
+	x=x0;
+	y=y0;
+
+	p=2*dy-dx;
+
+	for(int i =x0;i<=x1;i++)
+	{
+		if(p>=0)
+		{
+			putpixel(i,y,7);
+			y=y+1;
+			p+=2*dy-2*dx;
+			delay(100);
+		}
+		else
+		{
+			putpixel(i,y,7);
+			p+=2*dy;
+		}
+	}
+}
+
+int main()
+{
+    int x0, x1, y0,y1;
+    initwindow(1000,1000);
+	cout<<"Enter co-ordinates of first point: ";
+	cin>>x0>>y0;
+
+	cout<<"Enter co-ordinates of second point: ";
+	cin>>x1>>y1;
+	drawline(x0, y0, x1, y1);
+	outtextxy(10,10,"bibhu");
+    getch();
+	return 0;
+}
